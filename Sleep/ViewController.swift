83edbd0cff7604a88f11dab7f800eddf86dbc848
紫安絵理 <murayasueri@murayasuerinomacbook-ea.local>
2019-02-16 15:00:05 +0900

    Initial Commit

---
+++ Sleep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sleep
+//
+//  Created by 紫安絵理 on 2019/02/16.
+//  Copyright © 2019 紫安絵理. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SleepTests/SleepTests.swift
@@ -0,0 +1,34 @@
+//
+//  SleepTests.swift
+//  SleepTests
+//
+//  Created by 紫安絵理 on 2019/02/16.
+//  Copyright © 2019 紫安絵理. All rights reserved.
+//
+
+import XCTest
+@testable import Sleep
+
+class SleepTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
